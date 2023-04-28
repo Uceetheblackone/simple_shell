5b306d23d7f5cff7f0350a220d305639409b6f48
olisabelema <olisabelema@gmail.com>
2023-04-27 18:28:51 -0700

    correction

---
+++ defaultfunc.c
@@ -0,0 +1,119 @@
+#include "header.h"
+
+/**
+ * init_shell - function to initialize the shell program
+ */
+void init_shell(void)
+{
+	clear();
+	printf("\n\n***********************************************");
+	printf("\n\n****SIMPLE SHELL BY Bel & Juddy****");
+	printf("\n\n***********************************************");
+	printf("\n");
+	sleep(1);
+	clear();
+}
+
+/**
+ * prompt - function that write a prompt
+ *
+ * Return: 0 on sucess
+ */
+int prompt(void)
+{
+	char *prompt = "$ ";
+	ssize_t linecount = 0;
+
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		linecount = write(STDOUT_FILENO, prompt, 2);
+		if (linecount == -1)
+			exit(0);
+	}
+	return (0);
+}
+
+/**
+ * _read - reads stdin and stores it in a buffer
+ *
+ * Return: a pointer to the buffer
+ */
+char *_read(void)
+{
+	ssize_t readcount = 0;
+	size_t n = 0;
+	char *buffer = NULL;
+	int i = 0;
+
+	readcount = getline(&buffer, &n, stdin);
+	if (readcount == -1)
+	{
+		free(buffer);
+		if (isatty(STDIN_FILENO) != 0)
+			write(STDOUT_FILENO, "\n", 1);
+		exit(0);
+	}
+	if (buffer[readcount - 1] == '\n' || buffer[readcount - 1] == '\t')
+		buffer[readcount - 1] = '\0';
+	for (i = 0; buffer[i]; i++)
+	{
+		if (buffer[i] == '#' && buffer[i - 1] == ' ')
+		{
+			buffer[i] = '\0';
+			break;
+		}
+	}
+	return (buffer);
+}
+
+/**
+ * _strtoken - creates array of tokens based on user string
+ * @buffer: pointer to user string
+ *
+ * Return: pointer to array of user strings
+ */
+char **_strtoken(char *buffer)
+{
+	char *token;
+	int i = 0, wordcount = 0;
+	char *delimiter = " \n";
+	char **av;
+
+	wordcount = _splitstring(buffer);
+	if (!wordcount)
+		return (NULL);
+	av = malloc((wordcount + 1) * sizeof(char *));
+	if (av == NULL)
+		exit(1);
+	token = strtok(buffer, delimiter);
+	while (token != NULL)
+	{
+		av[i] = _strdup(token);
+		token = strtok(NULL, delimiter);
+		i++;
+	}
+	av[i] = NULL;
+	return (av);
+}
+
+/**
+ * _concat - concats user string with PATH member string and /
+ * @tmp: static array to store concatenated string
+ * @av: pointer to array of user strings
+ * @tok: pointer to PATH token
+ *
+ * Return: 0 on success
+ */
+char *_concat(char *tmp, char **av, char *tok)
+{
+	int len = 0;
+
+	_memset(tmp, 0, 256);
+	len = _strlen(tok) + _strlen(av[0]) + 2;
+	_strcat(tmp, tok);
+	_strcat(tmp, "/");
+	_strcat(tmp, av[0]);
+	tmp[len - 1] = '\0';
+	return (tmp);
+}
+
---
+++ main.c
@@ -0,0 +1,49 @@
+#include "header.h"
+
+/**
+ *main - Brain box for shell project
+ *
+ *
+ *Return:  return 0 (success)
+ */
+
+int main(void)
+{
+
+	char *pathbuffer = NULL;
+	char *copy = NULL;
+	char *buffer = NULL;
+	char *PATH = NULL;
+	char **argv;
+	int exitstatus = 0;
+
+	/* init_shell(); */
+	signal(SIGINT, SIG_IGN);
+	PATH = _getenv("PATH");
+	if (PATH == NULL)
+		return (EXIT_FAILURE);
+	while (1)
+	{
+		argv = NULL;
+		prompt();
+		buffer = _read();
+		if (*buffer != '\0')
+		{
+			argv = _strtoken(buffer);
+			if (argv == NULL)
+			{
+				free(buffer);
+				continue;
+			}
+			pathbuffer = _pathbuffer(argv, PATH, copy);
+			if (checkbuiltins(argv, buffer, exitstatus) == 1)
+				continue;
+			exitstatus = _forkprocess(argv, buffer, pathbuffer);
+		}
+		else
+			free(buffer);
+	}
+
+	return (EXIT_SUCCESS);
+}
+
